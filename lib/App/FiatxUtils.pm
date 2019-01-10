ea767e2fca06a3a011dddd71b66cc3d128dbe919
perlancar (on netbook-zenbook-ux305) <perlancar@gmail.com>
2019-01-10 10:58:22 +0700

    Initial commit

---
+++ lib/App/FiatxUtils.pm
@@ -0,0 +1,19 @@
+package App::FiatxUtils;
+
+# DATE
+# VERSION
+
+1;
+#ABSTRACT: Utilities related to Finance::Currency::FiatX
+
+=head1 DESCRIPTION
+
+This distributions provides the following command-line utilities which are CLI
+for functions in L<Finance::Currency::FiatX>:
+
+# INSERT_EXECS_LIST
+
+
+=head1 SEE ALSO
+
+=cut
